7caa5bb0fd061ac3098d09a7d69eea5212946018
root <nagaraja.hm.0001@gmail.com>
2021-12-27 22:00:01 +0100

    Update: submodule Mcu

--- LATEST/Mcu.h
+++ LATEST/Mcu.h
@@ -2,17 +2,17 @@
 
 class class_Mcu{
    public:
-      void InitFunction(void);
-      void InitRamSection(void);
-      void InitClock(void);
-      void DistributePllClock(void);
-      void GetPllStatus(void);
-      void GetResetReason(void);
-      void GetResetRawValue(void);
-      void PerformReset(void);
-      void SetMode(void);
-      void GetVersionInfo(void);
-      void GetRamState(void);
+      void InitFunction       (void);
+      void InitRamSection     (void);
+      void InitClock          (void);
+      void DistributePllClock (void);
+      void GetPllStatus       (void);
+      void GetResetReason     (void);
+      void GetResetRawValue   (void);
+      void PerformReset       (void);
+      void SetMode            (void);
+      void GetVersionInfo     (void);
+      void GetRamState        (void);
 };
 
 extern class_Mcu Mcu;
