30f622aaa438248d4dc45091dd1b7e7981c20856
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:08:35 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Mcu

--- LATEST/Mcu.cpp
+++ LATEST/Mcu.cpp
@@ -87,12 +87,10 @@ FUNC(void, MCU_CODE) module_Mcu::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgMcu;
          }
       }
