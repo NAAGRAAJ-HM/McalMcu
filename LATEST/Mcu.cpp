3fa0a51366fb341a7018425554637a525f52b728
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:35:38 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Mcu

--- LATEST/Mcu.cpp
+++ LATEST/Mcu.cpp
@@ -36,6 +36,13 @@ class module_Mcu:
    ,  public infMcu_EcuM
    ,  public class_Mcu_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_Mcu(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
