f862eeb1eb1d8131953cd1ff69585776a435d4a8
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-04 19:55:39 +0200

    Update: AUTOSAR basic software micro-controller abstraction driver module Mcu

---
+++ main_LATEST/McalMcu.cpp
@@ -0,0 +1,190 @@
+/******************************************************************************/
+/* File   : McalMcu.cpp                                                           */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "McalMcu.hpp"
+#include "infMcalMcu_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define MCU_AR_RELEASE_VERSION_MAJOR                                           4
+#define MCU_AR_RELEASE_VERSION_MINOR                                           3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(MCU_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible MCU_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(MCU_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible MCU_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_McalMcu, MCU_VAR) McalMcu;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, MCU_CODE) module_McalMcu::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, MCU_CONST,       MCU_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   MCU_CONFIG_DATA, MCU_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == McalMcu_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstMcalMcu_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == McalMcu_DevErrorDetect)
+         ServiceDet_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == McalMcu_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == McalMcu_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MCU_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::DeInitFunction(
+   void
+){
+#if(STD_ON == McalMcu_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == McalMcu_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == McalMcu_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MCU_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::MainFunction(
+   void
+){
+#if(STD_ON == McalMcu_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == McalMcu_InitCheck)
+   }
+   else{
+#if(STD_ON == McalMcu_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MCU_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::GetResetReason(
+   void
+){
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::InitRamSection(
+   void
+){
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::InitClock(
+   void
+){
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::DistributePllClock(
+   void
+){
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::GetPllStatus(
+   void
+){
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::GetResetRawValue(
+   void
+){
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::PerformReset(
+   void
+){
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::SetMode(
+   void
+){
+}
+
+FUNC(void, MCU_CODE) module_McalMcu::GetRamState(
+   void
+){
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
---
+++ main_LATEST/McalMcu_core.hpp
@@ -0,0 +1,68 @@
+#pragma once
+/******************************************************************************/
+/* File   : McalMcu_core.hpp                                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_McalMcu.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define MCU_CORE_FUNCTIONALITIES                                               \
+              FUNC(void, MCU_CODE) InitRamSection     (void);                  \
+              FUNC(void, MCU_CODE) InitClock          (void);                  \
+              FUNC(void, MCU_CODE) DistributePllClock (void);                  \
+              FUNC(void, MCU_CODE) GetPllStatus       (void);                  \
+              FUNC(void, MCU_CODE) GetResetReason     (void);                  \
+              FUNC(void, MCU_CODE) GetResetRawValue   (void);                  \
+              FUNC(void, MCU_CODE) PerformReset       (void);                  \
+              FUNC(void, MCU_CODE) SetMode            (void);                  \
+              FUNC(void, MCU_CODE) GetRamState        (void);                  \
+
+#define MCU_CORE_FUNCTIONALITIES_VIRTUAL                                       \
+      virtual FUNC(void, MCU_CODE) InitRamSection     (void) = 0;              \
+      virtual FUNC(void, MCU_CODE) InitClock          (void) = 0;              \
+      virtual FUNC(void, MCU_CODE) DistributePllClock (void) = 0;              \
+      virtual FUNC(void, MCU_CODE) GetPllStatus       (void) = 0;              \
+      virtual FUNC(void, MCU_CODE) GetResetReason     (void) = 0;              \
+      virtual FUNC(void, MCU_CODE) GetResetRawValue   (void) = 0;              \
+      virtual FUNC(void, MCU_CODE) PerformReset       (void) = 0;              \
+      virtual FUNC(void, MCU_CODE) SetMode            (void) = 0;              \
+      virtual FUNC(void, MCU_CODE) GetRamState        (void) = 0;              \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_McalMcu_Functionality{
+   public:
+      MCU_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
