8edb4b0382a9d8760a99398eee09c9cbdd8ca19b
Raajnaag HULIYAPURADA MATA <nagaraja.hm.0001@gmail.com>
2023-07-12 16:12:54 +0200

    Initial commit

--- RenesasRH850F1LR7F7010133AFP_R2023/McalMcu.c
+++ RenesasRH850F1LR7F7010133AFP_R2023/McalMcu.c
@@ -1,6 +1,5 @@
-#pragma once
 /******************************************************************************/
-/* File   : uC.h                                                              */
+/* File   : McalMcu.c                                                         */
 /*                                                                            */
 /* Author : Raajnaag HULIYAPURADA MATA                                        */
 /*                                                                            */
@@ -14,7 +13,7 @@
 /* certain responsibilities, if you distribute copies of the software, or if  */
 /* you modify it: responsibilities to respect the freedom of others.          */
 /*                                                                            */
-/* All rights reserved. Copyright Â© 1982 Raajnaag HULIYAPURADA MATA           */
+/* All rights reserved. Copyright © 1982 Raajnaag HULIYAPURADA MATA           */
 /*                                                                            */
 /* Always refer latest software version from:                                 */
 /* https://github.com/RaajnaagHuliyapuradaMata?tab=repositories               */
@@ -24,72 +23,41 @@
 /******************************************************************************/
 /* #INCLUDES                                                                  */
 /******************************************************************************/
+#include "Std_Types.h"
+
+#include "infMcalMcuSwcApplStartUp.h"
 
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-//#undef ReSim
-#define ReSim                                                                  1
-
-#ifndef ReSim
-#ifndef _GHS_PRAGMA_IO_TYPEDEF_
-#define _GHS_PRAGMA_IO_TYPEDEF_
-#define PRAGMA(x) _Pragma(#x)
-#define __READ volatile const
-#define __WRITE volatile
-#define __READ_WRITE volatile
-
-#define __IOREG(reg, addr, attrib, type) PRAGMA(ghs io reg addr) \
-extern attrib type reg;
-
-#define __IOREGARRAY(reg, array, addr, attrib, type) PRAGMA(ghs io reg addr) \
-extern attrib type reg[array];
-#endif
-#endif
 
 /******************************************************************************/
 /* MACROS                                                                     */
 /******************************************************************************/
-#define protected_write(preg,pstatus,reg,value)   do{\
-                                                  (preg)=0xa5u;\
-                                                  (reg)=(value);\
-                                                  (reg)=~(value);\
-                                                  (reg)=(value);\
-                                                  }while((pstatus)==1u)
 
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-typedef struct{
-   uint8 bit00:1;
-   uint8 bit01:1;
-   uint8 bit02:1;
-   uint8 bit03:1;
-   uint8 bit04:1;
-   uint8 bit05:1;
-   uint8 bit06:1;
-   uint8 bit07:1;
-}__bitf_T;
-
-typedef union{                                            /* IOR              */
-   uint32 UINT32;                                         /* 32-bit Access    */
-   uint16 UINT16[2];                                      /* 16-bit Access    */
-   uint8  UINT8[4];                                       /* 8-bit Access     */
-}__type9;
-
-typedef union{                                            /* IOR              */
-   uint32 UINT32;                                         /* 32-bit Access    */
-   uint16 UINT16[2];                                      /* 16-bit Access    */
-}__type16;
-
 
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
+static volatile VAR(uint32, MCALMCU_VAR_FAST) lvu32RegisterWakeupFactorControl;
+static volatile VAR(uint32, MCALMCU_VAR_FAST) lvu32RegisterWakeupFactor;
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
+FUNC(uint32, MCALMCU_CODE) infMcalMcuSwcApplStartUp_u32GetWakeupFactor(void){
+   return lvu32RegisterWakeupFactor;
+}
+
+FUNC(void, MCALMCU_CODE) infMcalMcuSwcApplStartUp_vSetWakeupFactor(
+   uint32 lu32Wuf
+){
+   lvu32RegisterWakeupFactor        = lu32Wuf;
+   lvu32RegisterWakeupFactorControl = !lvu32RegisterWakeupFactor;
+}
 
 /******************************************************************************/
 /* CONSTS                                                                     */
--- RenesasRH850F1LR7F7010133AFP_R2023/infMcalMcuSwcApplStartUp.h
+++ RenesasRH850F1LR7F7010133AFP_R2023/infMcalMcuSwcApplStartUp.h
@@ -1,6 +1,6 @@
 #pragma once
 /******************************************************************************/
-/* File   : uC.h                                                              */
+/* File   : infMcalMcuSwcApplStartUp.h                                        */
 /*                                                                            */
 /* Author : Raajnaag HULIYAPURADA MATA                                        */
 /*                                                                            */
@@ -14,7 +14,7 @@
 /* certain responsibilities, if you distribute copies of the software, or if  */
 /* you modify it: responsibilities to respect the freedom of others.          */
 /*                                                                            */
-/* All rights reserved. Copyright Â© 1982 Raajnaag HULIYAPURADA MATA           */
+/* All rights reserved. Copyright © 1982 Raajnaag HULIYAPURADA MATA           */
 /*                                                                            */
 /* Always refer latest software version from:                                 */
 /* https://github.com/RaajnaagHuliyapuradaMata?tab=repositories               */
@@ -24,71 +24,18 @@
 /******************************************************************************/
 /* #INCLUDES                                                                  */
 /******************************************************************************/
+#include "CompilerCfg_McalMcu.h"
 
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-//#undef ReSim
-#define ReSim                                                                  1
-
-#ifndef ReSim
-#ifndef _GHS_PRAGMA_IO_TYPEDEF_
-#define _GHS_PRAGMA_IO_TYPEDEF_
-#define PRAGMA(x) _Pragma(#x)
-#define __READ volatile const
-#define __WRITE volatile
-#define __READ_WRITE volatile
-
-#define __IOREG(reg, addr, attrib, type) PRAGMA(ghs io reg addr) \
-extern attrib type reg;
-
-#define __IOREGARRAY(reg, array, addr, attrib, type) PRAGMA(ghs io reg addr) \
-extern attrib type reg[array];
-#endif
-#endif
 
 /******************************************************************************/
 /* MACROS                                                                     */
 /******************************************************************************/
-#define protected_write(preg,pstatus,reg,value)   do{\
-                                                  (preg)=0xa5u;\
-                                                  (reg)=(value);\
-                                                  (reg)=~(value);\
-                                                  (reg)=(value);\
-                                                  }while((pstatus)==1u)
 
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
-/******************************************************************************/
-typedef struct{
-   uint8 bit00:1;
-   uint8 bit01:1;
-   uint8 bit02:1;
-   uint8 bit03:1;
-   uint8 bit04:1;
-   uint8 bit05:1;
-   uint8 bit06:1;
-   uint8 bit07:1;
-}__bitf_T;
-
-typedef union{                                            /* IOR              */
-   uint32 UINT32;                                         /* 32-bit Access    */
-   uint16 UINT16[2];                                      /* 16-bit Access    */
-   uint8  UINT8[4];                                       /* 8-bit Access     */
-}__type9;
-
-typedef union{                                            /* IOR              */
-   uint32 UINT32;                                         /* 32-bit Access    */
-   uint16 UINT16[2];                                      /* 16-bit Access    */
-}__type16;
-
-
-/******************************************************************************/
-/* OBJECTS                                                                    */
-/******************************************************************************/
-
-/******************************************************************************/
-/* FUNCTIONS                                                                  */
 /******************************************************************************/
 
 /******************************************************************************/
@@ -102,6 +49,12 @@ typedef union{                                            /* IOR              */
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+extern FUNC(uint32, MCALMCU_CODE) infMcalMcuSwcApplStartUp_u32GetWakeupFactor (void);
+extern FUNC(void,   MCALMCU_CODE) infMcalMcuSwcApplStartUp_vSetWakeupFactor   (uint32 ulWuf);
 
 /******************************************************************************/
 /* EOF                                                                        */
