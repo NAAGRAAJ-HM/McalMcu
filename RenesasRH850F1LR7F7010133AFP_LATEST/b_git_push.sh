1f6ee21f3589edea922560560606d80a588146cd
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2024-02-15 11:42:22 +0100

    Daily back-up

--- RenesasRH850F1LR7F7010133AFP_LATEST/b_git_push.sh
+++ RenesasRH850F1LR7F7010133AFP_LATEST/b_git_push.sh
@@ -1,4 +1,4 @@
 git add .
 git status
-git commit -m "AUTOSAR Migration Activities - EcuReal_vBcRsBpTpms_LATEST - McalMcu"
+git commit -m "AUTOSAR Migration Activities - rEcu_vBcRsBpTpms_LATEST - McalMcu"
 git push
